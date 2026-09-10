64da6c55933b3653506260c13de730d3143bd535
unknown <david.tenezacag.est@uets.edu.ec>
2026-09-10 01:45:02 -0500

    feat: completa bloque 02

--- bloque_2/src/bloque_2.ino
+++ bloque_2/src/bloque_2.ino
@@ -29,21 +29,29 @@ void setup() {
     // TODO 2.1: Arrancar la pantalla activando la bomba de carga interna (charge pump)
     // pasando la constante SSD1306_SWITCHCAPVCC y la dirección 0x3C.
     // Pregunta Guía: ¿Qué hace la bomba de carga con el voltaje de 3.3V?
-    // if (!display.begin(SSD1306_SWITCHCAPVCC, OLED_I2C_ADDR)) {
-    //     Serial.println("[OLED] ❌ ERROR: Pantalla no detectada en 0x3C.");
-    //     while (true); // Detiene la ejecución si hay falla física
-    // }
+     if (!display.begin(SSD1306_SWITCHCAPVCC, OLED_I2C_ADDR)) {
+        Serial.println("[OLED] ❌ ERROR: Pantalla no detectada en 0x3C.");
+        while (true); // Detiene la ejecución si hay falla física
+     }
+
+    
 
     Serial.println("[OLED] Pantalla SSD1306 inicializada [OK]");
 
     // TODO 2.2: Construir la cabecera visual en el buffer RAM:
     // 1. Limpiar el cuaderno borrador en RAM con display.clearDisplay();
+    display.clearDisplay();
     // 2. Definir color blanco con display.setTextColor(SSD1306_WHITE);
+    display.setTextColor(SSD1306_WHITE);
     // 3. Definir tamaño de letra 1 con display.setTextSize(1);
+    display.setTextSize(1);
     // 4. Ubicar el cursor en (X=8, Y=0) con display.setCursor(8, 0);
+    display.setCursor(8, 0);
     // 5. Escribir el título: display.println(">> ESP32 SISTEMA <<");
+    display.println(">> ESP32 SISTEMA <<");
     // 6. Trazar una línea horizontal en Y=10: display.drawLine(0, 10, 128, 10, SSD1306_WHITE);
     /* ESCRIBE TU CÓDIGO AQUÍ */
+    display.drawLine(0, 10, 128, 10, SSD1306_WHITE);
 
     // TODO 2.3: ¡LA ORDEN MÁGICA!
     // Pregunta Clave: Si solo escribiste en la memoria RAM, ¿por qué la pantalla sigue negra?
